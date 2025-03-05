e7abb610ef1950db3d1755485e6983d3696a6871
Moscolape <chunkums088@gmail.com>
2025-03-05 20:18:27 +0100

    chore: place pages under development

--- src/constants/assets.ts
+++ src/constants/assets.ts
@@ -1,3 +1,5 @@
 import notAvailableYet from "../assets/undraw_no-data_ig65.svg";
+import inProgress from "../assets/undraw_season-change_ohe6.svg";
 
-export {notAvailableYet}
+
+export {notAvailableYet, inProgress}
--- src/pages/donate.tsx
+++ src/pages/donate.tsx
@@ -1,7 +1,7 @@
 import PageWrapper from "../components/pageWrapper";
 import { useEffect } from "react";
 import initializeAOS from "../utils/aos-init";
-
+import { inProgress } from "../constants/assets";
 
 const Donate = () => {
   useEffect(() => {
@@ -11,7 +11,8 @@ const Donate = () => {
   return (
     <PageWrapper>
       <div className="flex flex-col justify-center items-center my-5 h-[60vh]">
-        <span className="block my-4">Nothing yet</span>
+        <img src={inProgress} alt="no-data" className="w-30 h-30" />
+        <span className="block my-4">Under development</span>
       </div>
     </PageWrapper>
   );
